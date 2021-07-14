e082aa8b6fea919ba3546d1e4561114d98f45975
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-07-14 14:32:09 -0700

    Update install-ov-dlw.sh

--- install-ov-dlw.sh
+++ install-ov-dlw.sh
@@ -1,9 +1,18 @@
 # Script to install docker, intel openvino and DL Workbench on Amazon Linux 2 EC2 instance
 
-sudo yum update -y && sudo yum groupinstall "Development Tools" &&
-sudo amazon-linux-extras install docker && sudo service docker start && sudo usermod -a -G docker ec2-user
+# Install dependencies and Docker
+sudo yum -y update && 
+sudo yum -y groupinstall "Development Tools" &&
+sudo amazon-linux-extras install -y docker && 
+sudo service docker start && 
+sudo usermod -a -G docker ec2-user
 
-sudo yum install python3 && sudo yum install python3-devel.x86_64 && pip3 install -U opencv-python-headless==4.2.0.32 && pip3 install openvino-python && python3 -m pip install -U openvino-workbench
+# Install OpenVINO and DL-Workbench
+sudo yum install -y python3 && 
+sudo yum install -y python3-devel.x86_64 &&
+pip3 install -U opencv-python-headless==4.2.0.32 &&
+pip3 install openvino-dev && 
+python3 -m pip install -U openvino-workbench
 
 
 
