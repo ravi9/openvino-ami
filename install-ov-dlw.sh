ce8684037a91b01cbe056c5bc2421ac0dbfbe62f
Pradeep Sakhamoori <pradeep.sakhamoori@intel.com>
2021-07-19 13:32:26 -0700

    Revert commit

--- install-ov-dlw.sh
+++ install-ov-dlw.sh
@@ -13,7 +13,7 @@ sudo yum install -y python3-devel.x86_64 &&
 pip3 install -U opencv-python-headless==4.2.0.32 &&
 pip3 uninstall -y opencv-python &&
 pip3 install openvino-dev && 
-pip3 install -U openvino-workbench
+python3 -m pip install -U openvino-workbench
 
 # Install Tensorflow, Pytorch, ONNX
 pip3 install tensorflow -U
