65ad94d12a59a69d03d35e48b47cd5339d6e3771
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-07-20 14:16:37 -0700

    Update test-pip-ov-install.sh

--- test-pip-ov-install.sh
+++ test-pip-ov-install.sh
@@ -12,3 +12,9 @@ mo \
 --model_name=googlenet-v1-tf \
 --input_shape=[1,224,224,3] \
 --input_model ./public/googlenet-v1-tf/inception_v1.frozen.pb
+
+## Test Python Imports
+
+python3 -c "from openvino.inference_engine import IENetwork, IECore, get_version as get_ov_version; print('openvino version: ' + get_ov_version())"
+
+python3 -c "from openvino.tools.benchmark.main import main"
