4f31ff46b4e443a014a8e018236f3fa0350793b8
Pradeep Sakhamoori <pradeep.sakhamoori@intel.com>
2021-07-22 16:21:14 -0700

    DL Workbench Docker image pull in background with cron job

--- start_dlw.sh
+++ start_dlw.sh
@@ -1,4 +1,4 @@
 # script to launch openvino-workbench container
-# Application accessed through webbrowser with http://127.0.0.1:5665/
+# Application accessed through webbrowser with http://ec2-public-ip:5665/
 
-openvino-workbench --image openvino/workbench:2021.4
+openvino-workbench --image openvino/workbench:2021.4 > /tmp/workbench.out 2>&1 &
