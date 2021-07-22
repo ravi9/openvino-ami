666e8b4a96e2401356f1d477017905db4c8c1964
Pradeep Sakhamoori <pradeep.sakhamoori@intel.com>
2021-07-22 16:23:28 -0700

    Add Docker.service start and cron job starting DL workbenc on http://ec2-public-ip:5665

--- add_cron_job.sh
+++ add_cron_job.sh
@@ -1,1 +1,1 @@
-(crontab -l; echo "@reboot su ec2-user -c $(/home/ec2-user/start_jupyter.sh)" ) | crontab -
+(crontab -l; echo "@reboot su ec2-user $(/home/ec2-user/start_jupyter.sh)"; $(sudo systemctl start docker); $(/home/ec2-user/start_dlw.sh)) | crontab -
