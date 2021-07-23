ebc2116a79aaaeac60b35bd6d050d543150d4599
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-07-23 16:54:56 -0700

    Updating OpenVINO scripts

---
+++ start_dlworkbench.sh
@@ -0,0 +1,4 @@
+# script to launch openvino-workbench container
+# Application accessed through webbrowser with http://<ec2-public-ip>:5665/
+
+openvino-workbench --image openvino/workbench:2021.4 >/tmp/workbench.out 2>&1 &
--- test-pip-ov-install.sh
+++ test-pip-ov-install.sh
@@ -1,14 +1,19 @@
-
 omz_downloader --name googlenet-v1-tf
 
 omz_converter --name googlenet-v1-tf
 
-benchmark_app -m public/googlenet-v1-tf/FP32/googlenet-v1-tf.xml
+benchmark_app -m public/googlenet-v1-tf/FP32/googlenet-v1-tf.xml -niter 1
 
 mo \
---framework=tf \
---data_type FP32 \
---output_dir ./public/googlenet-v1-tf/FP32 \
---model_name=googlenet-v1-tf \
---input_shape=[1,224,224,3] \
---input_model ./public/googlenet-v1-tf/inception_v1.frozen.pb
+    --framework=tf \
+    --data_type FP32 \
+    --output_dir ./public/googlenet-v1-tf/FP32 \
+    --model_name=googlenet-v1-tf \
+    --input_shape=[1,224,224,3] \
+    --input_model ./public/googlenet-v1-tf/inception_v1.frozen.pb
+
+## Test Python Imports
+
+python3 -c "from openvino.inference_engine import IENetwork, IECore, get_version as get_ov_version; print('openvino version: ' + get_ov_version())"
+
+python3 -c "from openvino.tools.benchmark.main import main"
