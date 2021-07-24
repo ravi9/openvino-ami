cc5751c10fe5e8e7fed5a623fd68a3dde1e8bb04
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-07-23 18:42:46 -0700

    Updating OpenVINO scripts

--- start_dlworkbench.sh
+++ start_dlworkbench.sh
@@ -5,4 +5,4 @@
 docker rm workbench
 
 # Start OpenVINO DL Workbench
-/home/ec2-user/.local/bin/openvino-workbench --image openvino/workbench:2021.4 >/tmp/ov-workbench.log 2>&1 &
+/home/ec2-user/.local/bin/openvino-workbench --image openvino/workbench:2021.4 --enable-authentication >/tmp/ov-workbench.log 2>&1 &
