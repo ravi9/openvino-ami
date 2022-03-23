d06ed265c1fd890c4fa9a8ce9aa37dee2d521030
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2022-03-23 22:08:06 +0530

    Update start_dlworkbench.sh

--- start_dlworkbench.sh
+++ start_dlworkbench.sh
@@ -1,12 +1,13 @@
 # Script to launch openvino-workbench container
 # OpenVINO DL Workbench Application accessed through web browser with http://<ec2-public-ip>:5665/
 
+OV_VERSION=2022.1.0
 # Remove any existing workbench container
 docker rm workbench
 
 # Start OpenVINO DL Workbench
 /home/ec2-user/.local/bin/openvino-workbench \
-        --image openvino/workbench:2021.4.2 \
+        --image openvino/workbench:$OV_VERSION \
         --enable-authentication \
         --assets-directory /home/ec2-user/.workbench \
         >/tmp/ov-workbench.log 2>&1 &
